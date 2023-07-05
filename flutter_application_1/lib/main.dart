e32d5be1fc5a72d62c76257978ea6007c3e85743
harsh1ni <hjayaprakash1@student.ohlone.edu>
2023-07-04 19:13:49 -0700

    New downloads

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
