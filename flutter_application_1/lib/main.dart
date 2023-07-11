2e3d519461b757af1f91b65a3529038d22cac2d7
harsh1ni <hjayaprakash1@student.ohlone.edu>
2023-07-10 19:22:38 -0700

    Imported aaminah's code in

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -46,3 +46,115 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          //title: const Text('Second Route'),
+          ),
+      body: Column(
+          mainAxisSize: MainAxisSize.max,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.pop(context);
+            //   },
+            //   child: const Text('Back'),
+            // ),
+
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.pop(context);
+            //   },
+            //   child: const Text('Back'),
+            // ),
+
+            Image(image: AssetImage('images/logo2.png')),
+
+            // Image(image: AssetImage('images/logo.png')),
+
+            //Image(image: AssetImage('images/WelcomeLegal.png')),
+
+            Align(
+              alignment: Alignment.center,
+              child: Image(image: AssetImage('images/WelcomeLegal.png')),
+            ),
+
+            Text('     '),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ThirdRoute()),
+                );
+                //Navigator.push(context.MaterialPageRoute(builder:(context)=>seconscreen))
+              },
+              child: const Text('Social Security Form'),
+            ),
+
+            // ElevatedButton(
+            //   onPressed: () {},
+            //   child: const Text('Social Security Form'),
+            // ),
+          ]),
+    );
+  }
+}
+
+class ThirdRoute extends StatelessWidget {
+  const ThirdRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          //title: const Text('Second Route'),
+          ),
+      body: Column(
+          mainAxisSize: MainAxisSize.max,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Align(
+            //   alignment: Alignment.topLeft,
+            //   child: ElevatedButton(
+            //     onPressed: () {
+            //       Navigator.pop(context);
+            //     },
+            //     child: const Text('Back'),
+            //   ),
+            // ),
+            Expanded(
+              child: Image(image: AssetImage('images/sstop.png')),
+            ),
+            Expanded(
+              child: Image(image: AssetImage('images/ssbottom.png')),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Next'),
+              ),
+            ),
+
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.pop(context);
+            //   },
+            //   child: const Text('Back'),
+            // ),
+
+            // ElevatedButton(
+            //   onPressed: () {},
+            //   child: const Text('Social Security Form'),
+            // ),
+          ]),
+    );
+  }
+}
