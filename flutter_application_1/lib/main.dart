e2d73dde74b3c7056b430e7c9c2dc680f84e61ab
harsh1ni <hjayaprakash1@student.ohlone.edu>
2023-07-11 00:35:41 -0700

    complete aaminah's code

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,132 +1,176 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-void navigateToSecond(BuildContext context) {
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => const SecondRoute()),
-  );
-}
-
-void navigateToThird(BuildContext context) {
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => const ThirdRoute()),
-  );
-}
-
-void navigateToFourth(BuildContext context) {
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => const FourthRoute()),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          centerTitle: true,
-          title: const Text('LegalEase'),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
         ),
-        backgroundColor: Colors.white,
-        body: Stack(
-          children: [
-            Center(
-              child: Image.asset('images/lelogo.png'),
-            ),
-            Positioned(
-              bottom: 16,
-              right: 16,
-              child: ElevatedButton(
-                onPressed: () => navigateToSecond(context),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.grey[300],
-                ),
-                child: const Text('Next'),
-              ),
-            ),
-          ],
-        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Column(
+        // mainAxisSize: MainAxisSize.max,
+        //crossAxisAlignment: CrossAxisAlignment.center,
+        //mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Text('LEAGALEASE:'),
+          //Text(appState.current.asLowerCase),
+
+          Image(image: AssetImage('images/logo1.png')),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondRoute()),
+              );
+              //Navigator.push(context.MaterialPageRoute(builder:(context)=>seconscreen))
+            },
+            child: Text('Lets get started!'),
+          ),
+        ],
       ),
     );
   }
 }
 
 class SecondRoute extends StatelessWidget {
-  const SecondRoute({Key? key}) : super(key: key);
+  const SecondRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+          //title: const Text('Second Route'),
+          ),
       body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Image(image: AssetImage('images/logo2.png')),
-          Align(
-            alignment: Alignment.center,
-            child: Image(image: AssetImage('images/WelcomeLegal.png')),
-          ),
-          Text('     '),
-          ElevatedButton(
-            onPressed: () => navigateToThird(context),
-            child: const Text('Social Security Form'),
-          ),
-        ],
-      ),
+          mainAxisSize: MainAxisSize.max,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.pop(context);
+            //   },
+            //   child: const Text('Back'),
+            // ),
+
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.pop(context);
+            //   },
+            //   child: const Text('Back'),
+            // ),
+
+            Image(image: AssetImage('images/logo2.png')),
+
+            // Image(image: AssetImage('images/logo.png')),
+
+            //Image(image: AssetImage('images/WelcomeLegal.png')),
+
+            Align(
+              alignment: Alignment.center,
+              child: Image(image: AssetImage('images/WelcomeLegal.png')),
+            ),
+
+            Text('     '),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ThirdRoute()),
+                );
+                //Navigator.push(context.MaterialPageRoute(builder:(context)=>seconscreen))
+              },
+              child: const Text('Social Security Form'),
+            ),
+
+            // ElevatedButton(
+            //   onPressed: () {},
+            //   child: const Text('Social Security Form'),
+            // ),
+          ]),
     );
   }
 }
 
 class ThirdRoute extends StatelessWidget {
-  const ThirdRoute({Key? key}) : super(key: key);
+  const ThirdRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+          //title: const Text('Second Route'),
+          ),
       body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Expanded(
-            child: Image(image: AssetImage('images/sstop.png')),
-          ),
-          Expanded(
-            child: Image(image: AssetImage('images/ssbottom.png')),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: ElevatedButton(
-              onPressed: () => navigateToFourth(context),
-              child: const Text('Next'),
+          mainAxisSize: MainAxisSize.max,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Align(
+            //   alignment: Alignment.topLeft,
+            //   child: ElevatedButton(
+            //     onPressed: () {
+            //       Navigator.pop(context);
+            //     },
+            //     child: const Text('Back'),
+            //   ),
+            // ),
+            Expanded(
+              child: Image(image: AssetImage('images/sstop.png')),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+            Expanded(
+              child: Image(image: AssetImage('images/ssbottom.png')),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Next'),
+              ),
+            ),
 
-class FourthRoute extends StatelessWidget {
-  const FourthRoute({Key? key}) : super(key: key);
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.pop(context);
+            //   },
+            //   child: const Text('Back'),
+            // ),
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Fourth Route'),
-      ),
+            // ElevatedButton(
+            //   onPressed: () {},
+            //   child: const Text('Social Security Form'),
+            // ),
+          ]),
     );
   }
 }
