b2fc653a29cb5a4370ae25b9a1ccf536025632f4
harsh1ni <hjayaprakash1@student.ohlone.edu>
2023-07-12 09:44:49 -0700

    issues w project directory + spaces/colors updates

---
+++ flutter_application_1/lib/utils/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+//Monochromatic/Main pallette
+const white = Color(0xFFFFFFFF);
+const darkGrey = Color(0xFF595959);
+const lightGrey = Color(0xFFB2B2B2);
+const black = Color(0xFF000000);
+
+//Semantic
+const standard = Color(0xFF00A6FB);
+const error = Color(0xFFEF476F);
+const success = Color(0xFF06A6D0);
+const warning = Color(0xFFFFD166);
