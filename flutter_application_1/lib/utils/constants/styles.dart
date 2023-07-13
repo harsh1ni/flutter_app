d225783ea2d3eafd787a3b3890b1cd5f66dc93fe
harsh1ni <hjayaprakash1@student.ohlone.edu>
2023-07-12 22:03:14 -0700

    added a few changes to colors and styles

---
+++ flutter_application_1/lib/utils/constants/styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'colors.dart';
+
+//The font style martel will substitute for Malayalam MN
+
+
+var title = Text("Demo Lesson",
+    style: GoogleFonts.martel(
+        textStyle: const TextStyle(
+            fontSize: 15, fontWeight: FontWeight.bold, color: black)));
+
+var header = Text("Demo Lesson",
+    style: GoogleFonts.montserrat(
+        textStyle: const TextStyle(
+            fontSize: 16, fontWeight: FontWeight.bold, color: white)));
+
